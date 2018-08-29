30d1e559e4049be3d707600e3af674b61e2ab60e
Suhail Shergill <suhailshergill@gmail.com>
2018-08-29 15:21:37 -0400

    provorg: add copyright

--- provorg.el
+++ provorg.el
@@ -1,3 +1,32 @@
+;;; provorg.el --- a simple provisioner                     -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2018 Suhail Shergill
+
+;; Author: Suhail Shergill
+;; Keywords: lisp
+;; Version: 0.0.1
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; A simple provisioning system built on top of org-mode.
+
+;;; Code:
+
+;; code goes here
+
 (require 'ob)
 
 (eval-after-load 'ob
@@ -231,3 +260,5 @@
 ;; (setq provorg/host "chaos")
 
 (provide 'provorg)
+;;; provorg.el ends here
+
